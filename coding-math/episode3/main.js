e05aa6e242506cc17cfb79c66493fff2f31731fb
dalakoti07 <saurabhdalakoti07@gmail.com>
2023-02-01 23:25:59 +0530

    added fade in and fade out; transform shape animation using SIN wave

--- coding-math/episode3/main.js
+++ coding-math/episode3/main.js
@@ -31,7 +31,61 @@ window.onload = function () {
         requestAnimationFrame(bounceAnimation);
     }
 
+    /*var centerY = height * 0.5,
+        centerX = width * 0.5,
+        baseRadius = 200,
+        offset = 50,
+        speed = 0.1,
+        angle = 0;
 
+    transformRadius()
+
+    /!**
+     * this function will transform its shape radius
+     *!/
+    function transformRadius(){
+        let radius = baseRadius + Math.sin(angle) * offset
+
+        // clear canvas
+        context.clearRect(0,0,width, height);
+        context.beginPath();
+        // create circle
+        context.arc(centerX,centerY,radius,0,2*Math.PI , false);
+        context.fill();
+
+        angle+=speed;
+
+        requestAnimationFrame(transformRadius);
+    }*/
+
+    /*var centerY = height * 0.5,
+        centerX = width * 0.5,
+        baseAlpha = 0.5,
+        offset = 0.2,
+        speed = 0.1,
+        angle = 0;
+
+    opacityAnimation()
+
+    /!**
+     * this function will show effect
+     *!/
+    function opacityAnimation(){
+        let alpha = baseAlpha + Math.sin(angle) * offset;
+
+        context.fillStyle = "rgba(0,0,0," + alpha + ")";
+
+        // clear canvas
+        context.clearRect(0,0,width, height);
+        context.beginPath();
+        // create circle
+        context.arc(centerX,centerY,100,0,2*Math.PI , false);
+        context.fill();
+
+        angle+=speed;
+
+        requestAnimationFrame(opacityAnimation);
+    }*/
 
 }
 
