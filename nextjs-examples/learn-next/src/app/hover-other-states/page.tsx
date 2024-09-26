077d911c9c0c3483a44dd77c6116d5d91fd6607f
dalakoti07 <saurabhdalakoti07@gmail.com>
2024-09-26 09:42:55 +0530

    tailwind css

---
+++ nextjs-examples/learn-next/src/app/hover-other-states/page.tsx
@@ -0,0 +1,12 @@
+
+export default function HoverNOtherStates() {
+    return (
+        <div className="flex flex-col m-10">
+            <button className="bg-sky-500 hover:bg-sky-700 width-30">
+                Save changes
+            </button>
+            
+            <p>Remove padding using </p>
+        </div>
+    );
+}
